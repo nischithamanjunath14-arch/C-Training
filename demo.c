022a6aa2384f7625f4e56b955d2c8633912f5907
nischithamanjunath14-arch <nischithamanjunath14@gmail.com>
2026-07-03 15:54:33 +0530

    DAY 1

---
+++ demo.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+    int i;
+    printf("Enter a  number: ");
+    scanf("%d", &i);
+    printf("You entered: %d", i);
+}
---
+++ demo.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    cout << "Enter a number: ";
+    cin >> i;
+    cout << "You entered: " << i << endl;
+    return 0;
+}
